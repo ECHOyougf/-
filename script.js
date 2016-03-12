9ae45398ed35ddd125775aad868d13948c26a94a
yzz <1163558472@qq.com>
2016-03-12 17:00:08 +0800

    mybf

---
+++ script.js
@@ -0,0 +1,35 @@
+var wordStore = [
+  {"你好": "你好啊"},
+  {"天气": "天气确实不错"},
+  {"身体": "最近好多了"},
+  {"hello": "hi"},
+  {"hi": "hello"},
+  {"不": "为什么呢"},
+  {"你是": "我是小正"},
+  {"唉": "一切都会好起来的"},
+  {"哎": "一切都会好起来的"},
+  {"哈": "哈哈"},
+  {"爱": "嗯。我爱你"},
+];
+var asker = document.getElementById("asker");
+var answer = document.getElementById("answer");
+asker.onkeyup = function () {
+  var txt = asker.value;
+  if (txt) {
+    for (var i = 0; i < wordStore.length; i++) {
+      for (var attr in wordStore[i]) {/*attr是属性，wordStore[0][attr]获取属性值*/
+        var result = txt.match(attr);
+        /*if (!result ) {
+          {
+            answer.innerHTML = "啊，我现在还是只蠢萌的兔子。真是不好意思，我们聊聊别的吧，嗯？";
+          }
+        }*/
+        if (result) {
+          answer.innerHTML = "小正："+wordStore[i][attr];
+        }
+      }
+    }
+  } else {
+    answer.innerHTML = "";
+  }
+}
